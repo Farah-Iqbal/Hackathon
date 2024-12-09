c8840cac79c3c62184168877d1e43ae5078441ae
Farah-Iqbal <rahmeenzafar78@gmail.com>
2024-12-09 12:39:09 -0800

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      letterSpacing:{
+        '3pct':'3%'
+      },
+      fontFamily:{
+        poppins:['Poppins','sans-serif'],
+        inter:['Inter','sans-serif'],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
